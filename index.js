f4d233e9e294de61a9f835d647d848d300c53001
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-14 21:37:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ class Tree {
 
 class Deciduous extends Tree {
   constructor(species, name) {
-    super.species = species
+    super(species) = species
     this.name = name
   }
   static definition(){
